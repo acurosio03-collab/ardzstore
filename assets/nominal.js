7cc0cb01858da094e997e22659abe6da5a991646
acurosio03-collab <acurosio03@gmail.com>
2026-07-26 21:16:12 +0700

    Create nominal.js

---
+++ assets/nominal.js
@@ -0,0 +1,86 @@
+let dataNominal = JSON.parse(localStorage.getItem("nominal")) || [];
+
+const form = document.getElementById("nominalForm");
+const list = document.getElementById("listNominal");
+
+function formatRupiah(angka){
+    return "Rp " + Number(angka).toLocaleString("id-ID");
+}
+
+function tampilkan(){
+
+    list.innerHTML="";
+
+    if(dataNominal.length===0){
+        list.innerHTML="<p>Belum ada data nominal.</p>";
+        return;
+    }
+
+    dataNominal.forEach((item,index)=>{
+
+        let hargaJual =
+        Number(item.supplier)+Number(item.margin);
+
+        list.innerHTML+=`
+
+<div class="nominal-card">
+
+<h3>${item.game}</h3>
+
+<b>${item.produk}</b>
+
+<p>Supplier : ${formatRupiah(item.supplier)}</p>
+
+<p>Margin : ${formatRupiah(item.margin)}</p>
+
+<h2>${formatRupiah(hargaJual)}</h2>
+
+<button onclick="hapus(${index})">
+🗑 Hapus
+</button>
+
+</div>
+
+`;
+
+    });
+
+}
+
+form.addEventListener("submit",function(e){
+
+e.preventDefault();
+
+const game=document.getElementById("game").value;
+const produk=document.getElementById("produk").value;
+const supplier=document.getElementById("supplier").value;
+const margin=document.getElementById("profit").value;
+
+dataNominal.push({
+
+game,
+produk,
+supplier,
+margin
+
+});
+
+localStorage.setItem("nominal",JSON.stringify(dataNominal));
+
+form.reset();
+
+tampilkan();
+
+});
+
+function hapus(index){
+
+dataNominal.splice(index,1);
+
+localStorage.setItem("nominal",JSON.stringify(dataNominal));
+
+tampilkan();
+
+}
+
+tampilkan();
