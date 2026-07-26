177197ec1050ae78b6b6c4c290cf96a20075bfd5
acurosio03-collab <acurosio03@gmail.com>
2026-07-26 09:39:58 +0700

    Update shop.js

--- shop.js
+++ shop.js
@@ -3,18 +3,17 @@ const products = JSON.parse(localStorage.getItem("products")) || [];
 const container = document.getElementById("shop-products");
 
 if (container) {
+    container.className = "game-list";
     container.innerHTML = "";
 
     products.forEach(item => {
         container.innerHTML += `
-        <div class="game-card">
+        <div class="card">
+            <img src="logo.jpeg" alt="${item.nama}">
             <h3>${item.nama}</h3>
-            <p>Harga: Rp ${item.harga}</p>
-            <p>Status: ${item.status}</p>
-
-            <a href="https://wa.me/6282295071107?text=Saya ingin topup ${item.nama}"
-               class="buy-btn">
-               Beli
+            <p>Rp ${item.harga}</p>
+            <a href="https://wa.me/6282295071107?text=Halo, saya ingin top up ${item.nama}" class="buy-btn">
+                BELI
             </a>
         </div>
         `;
