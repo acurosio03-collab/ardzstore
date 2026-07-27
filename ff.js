755f6364287768a163b0917e14021a711c2d8d20
acurosio03-collab <acurosio03@gmail.com>
2026-07-27 18:35:42 +0700

    Create ff.js

---
+++ ff.js
@@ -0,0 +1,84 @@
+let produk = "";
+let harga = 0;
+
+function pilihProduk(nama,total){
+
+    produk = nama;
+    harga = total;
+
+    document.getElementById("produk").innerHTML = nama;
+    document.getElementById("total").innerHTML =
+        "Rp " + total.toLocaleString("id-ID");
+}
+
+function loadFF(){
+
+    let data = JSON.parse(localStorage.getItem("nominal")) || [];
+
+    let html = "";
+
+    data.forEach(item=>{
+
+        if(item.game=="Free Fire"){
+
+            let jual =
+                Number(item.supplier)+Number(item.profit);
+
+            html += `
+            <div class="card"
+            onclick="pilihProduk('${item.produk}',${jual})">
+
+                <h3>${item.produk}</h3>
+
+                <p>Rp ${jual.toLocaleString("id-ID")}</p>
+
+            </div>
+            `;
+
+        }
+
+    });
+
+    document.getElementById("ff-products").innerHTML = html;
+
+}
+
+loadFF();
+
+function checkoutFF(){
+
+    let userid=document.getElementById("userid").value;
+    let payment=document.getElementById("payment").value;
+    let wa=document.getElementById("nomorwa").value;
+
+    if(userid==""){
+        alert("Masukkan User ID!");
+        return;
+    }
+
+    if(produk==""){
+        alert("Pilih Diamond terlebih dahulu!");
+        return;
+    }
+
+    let admin="6282295071107";
+
+    let pesan=`Halo Admin ARDZ STORE
+
+Saya ingin Top Up Free Fire
+
+Game : Free Fire
+User ID : ${userid}
+Produk : ${produk}
+Total : Rp ${harga.toLocaleString("id-ID")}
+Pembayaran : ${payment}
+Nomor WA : ${wa}
+
+Mohon diproses.`;
+
+    window.open(
+        "https://wa.me/"+admin+"?text="+encodeURIComponent(pesan),
+        "_blank"
+    );
+
+}
