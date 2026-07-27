11cff4e200466d87414e9351a40080506a72f738
acurosio03-collab <acurosio03@gmail.com>
2026-07-27 19:25:04 +0700

    Update valorant.js

--- valorant.js
+++ valorant.js
@@ -65,16 +65,35 @@ function checkoutvalorant(){
 
     let pesan=`Halo Admin ARDZ STORE
 
-Saya ingin Top Up valorant
+// ========================
+// SIMPAN PESANAN
+// ========================
 
-Game : valorant
-User ID : ${userid}
-Produk : ${produk}
-Total : Rp ${harga.toLocaleString("id-ID")}
-Pembayaran : ${payment}
-Nomor WA : ${wa}
+let orders = JSON.parse(localStorage.getItem("orders")) || [];
 
-Mohon diproses.`;
+orders.push({
+
+    game: "valorant",
+
+    userid: userid,
+
+    zoneid: zoneid,
+
+    produk: produk,
+
+    total: harga,
+
+    payment: payment,
+
+    wa: wa,
+
+    status: "Menunggu",
+
+    waktu: new Date().toLocaleString("id-ID")
+
+});
+
+localStorage.setItem("orders", JSON.stringify(orders));
 
     window.open(
         "https://wa.me/"+admin+"?text="+encodeURIComponent(pesan),
