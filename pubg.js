211ea2cb760193cfd412c360c33cde65e65bd129
acurosio03-collab <acurosio03@gmail.com>
2026-07-30 21:16:27 +0700

    Update pubg.js

--- pubg.js
+++ pubg.js
@@ -121,6 +121,8 @@ function $(id){
 ========================================== */
 
 function renderProducts(){
+console.log("RENDER PUBG JALAN");
+console.log(products);
 
     const productList = $("productList");
 
@@ -858,6 +860,3 @@ document.addEventListener("DOMContentLoaded",()=>{
 
 });
 }
-function renderProducts(){
-   console.log("RENDER PUBG JALAN");
-console.log(products);
